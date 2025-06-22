af901c86493433d0cab5bc9ec43c95b063d38293
navid <navidrahman92@gmail.com>
2025-06-22 14:39:25 +0600

    Added core constants, error handling, and network info

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String baseUrl = 'https://api.github.com';
+  static const String searchRepositories = '/search/repositories';
+  static const String searchQuery = 'Flutter';
+  static const int perPage = 50;
+}
---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,18 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkInfoImpl(this.connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    final results = await connectivity.checkConnectivity();
+    // If all results are 'none', then not connected
+    return results.any((r) => r != ConnectivityResult.none);
+  }
+}
